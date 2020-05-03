06efa212bdd01ff185fb23241be25a2d6084e379
Yeshwanth Reddy <notyeshwanthreddy@gmail.com>
2020-05-04 02:46:24 +0530

    Added GPT2 language model with 5 word suggestions. Need to add probabilities of suggestions. Need to fix HTML Looks for gpt2 results.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 import torch
 import numpy as np
-from transformers import BertTokenizer, BertForMaskedLM
+from transformers import BertTokenizer, BertForMaskedLM, GPT2Tokenizer, GPT2LMHeadModel
 import nltk
 
 app = Flask(__name__)
@@ -15,58 +15,52 @@ bert_model = BertForMaskedLM.from_pretrained('bert-base-uncased', output_attenti
 bert_model.eval()
 
 # GPT2 - Language Modelling
-gpt_tokenizer = BertTokenizer.from_pretrained('')
-gpt_model = BertForMaskedLM.from_pretrained('', output_attentions=True)
+gpt_tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+gpt_model = GPT2LMHeadModel.from_pretrained('gpt2', output_attentions=True)
 gpt_model.eval()
 
-# @app.route('/complete_sentence', methods=['POST'])
-# def LM_predict():
-# 	sentence_orig = request.form.get('text')
-# 	if '____' not in sentence_orig:
-# 		return sentence_orig
 
-# 	sentence = sentence_orig.replace('____', 'MASK')
-# 	tokens = nltk.word_tokenize(sentence)
-# 	sentences = nltk.sent_tokenize(sentence)
-# 	sentence = " [SEP] ".join(sentences)
-# 	sentence = "[CLS] " + sentence + " [SEP]"
-# 	tokenized_text = gpt_tokenizer.tokenize(sentence)
-# 	masked_index = tokenized_text.index('mask')
-# 	tokenized_text[masked_index] = "[MASK]"
-# 	indexed_tokens = gpt_tokenizer.convert_tokens_to_ids(tokenized_text)
+@app.route('/get_next_word', methods=['POST'])
+def LM_predict():
+	sentence_orig = request.form.get('text')
 
-# 	segments_ids = []
-# 	sentences = sentence.split('[SEP]')
-# 	for i in range(len(sentences)-1):
-# 		segments_ids.extend([i]*len(sentences[i].strip().split()))
-# 		segments_ids.extend([i])
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	
+	encoded_prompt = gpt_tokenizer.encode(sentence_orig, add_special_tokens=False, return_tensors="pt")
+	encoded_prompt = encoded_prompt.to(device)
 
-# 	tokens_tensor = torch.tensor([indexed_tokens])
-# 	segments_tensors = torch.tensor([segments_ids])
+	if encoded_prompt.size()[-1] == 0:
+		input_ids = None
+	else:
+		input_ids = encoded_prompt
 
-# 	with torch.no_grad():
-# 	    outputs = gpt_model(tokens_tensor, token_type_ids=segments_tensors)
-# 	    predictions = outputs[0]
-# 	    attention = outputs[-1]
 
-# 	dim = attention[2][0].shape[-1]*attention[2][0].shape[-1]
-# 	a = attention[2][0].reshape(12, dim)
-# 	b = a.mean(axis=0)
-# 	c = b.reshape(attention[2][0].shape[-1],attention[2][0].shape[-1])
-# 	avg_wgts = c[masked_index]
-# 	#print (avg_wgts, tokenized_text)
-# 	focus = [tokenized_text[i] for i in avg_wgts.argsort().tolist()[::-1] if tokenized_text[i] not in ['[SEP]', '[CLS]', '[MASK]']][:5]
+	output_sequences = gpt_model.generate(
+		input_ids=input_ids,
+		max_length= 1 + len(encoded_prompt[0]),
+		temperature=1.0,
+		top_k=50,
+		top_p=1.0,
+		repetition_penalty=1.0,
+		do_sample=True,
+		num_return_sequences=5,
+	)
 
-# 	# for layer in range(12):
-# 	# 	weights_layer = np.array(attention[0][0][layer][masked_index])
-# 	# 	print (weights_layer, tokenized_text)
-# 	# 	print (weights_layer.argsort()[-3:][::-1])
-# 	# 	print ()
-# 	predicted_index = torch.argmax(predictions[0, masked_index]).item()
-# 	predicted_token = gpt_tokenizer.convert_ids_to_tokens([predicted_index])[0]
-# 	for f in focus:
-# 		sentence_orig = sentence_orig.replace(f, '<font color="blue">'+f+'</font>')
-# 	return sentence_orig.replace('____', '<font color="red"><b><i>'+predicted_token+'</i></b></font>')
+	output = []
+	for generated_sequence in output_sequences:
+		# print("=== GENERATED SEQUENCE {} ===".format(generated_sequence_idx + 1))
+		generated_sequence = generated_sequence.tolist()
+		# Decode text
+		text = gpt_tokenizer.decode(generated_sequence, clean_up_tokenization_spaces=True)
+		# Remove all text after the stop token
+		text = text[: text.find(None) if None else None]
+		# Add the prompt at the beginning of the sequence. Remove the excess text that was used for pre-processing
+		total_sequence = (
+			text[len(gpt_tokenizer.decode(encoded_prompt[0], clean_up_tokenization_spaces=True)) :]
+		)
+		output.append(total_sequence)
+
+	return output
 
 
 @app.route('/fillblanks', methods=['POST'])
@@ -75,23 +69,30 @@ def MLM_predict():
 	if '__' not in sentence_orig:
 		return sentence_orig
 
+	# formatting sentence
 	sentence = sentence_orig.replace('__', '[MASK]')
 
-	tokenized_text = bert_tokenizer.tokenize(sentence)
+	# tokenising sentence
+	# tokenized_text = bert_tokenizer.tokenize(sentence)
+	# encoding sentence
 	input_ids = torch.tensor(bert_tokenizer.encode(sentence)).unsqueeze(0)
+	# Getting the outputs
 	outputs = bert_model(input_ids)[0]
-
+	
+	# Getting the index of the masked words in sentence
 	masked_index = []
 	for i,j in enumerate(input_ids[0]):
 		if j == bert_tokenizer.convert_tokens_to_ids("[MASK]"):
 			masked_index.append(i)
 
+	# Getting suggested words and their probabilities for each masked word
 	answers = []
 	for index in masked_index:
 		predicted_prob,predicted_index = torch.topk(torch.softmax(outputs[0][index],dim=0),top)
 		predicted_tokens = bert_tokenizer.convert_ids_to_tokens(predicted_index)
 		answers.append(list(zip(predicted_tokens,predicted_prob.tolist())))
 	
+	# Preparing the output for HTML
 	for answer in answers:
 		popup = [str(word)+": "+str(round(value, 4)) for word,value in answer]
 		popup = ("<br>").join(popup)
